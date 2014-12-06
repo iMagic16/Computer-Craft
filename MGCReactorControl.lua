2391d333b3e7946c5552c0894ac0e1b1880a7b6c
James <jameswalsh5683@gmail.com>
2014-12-06 02:11:29 +0000

    lots of changes, too many to list

--- MGCReactorControl.lua
+++ MGCReactorControl.lua
@@ -1,19 +1,19 @@
---Magic's reactor control
+--Magic's reactor monitor
 print()
 print()
 print("---------------------------")
 sleep(0.15)
-print("| Magic's Reactor Control |")
+print("| Magic's Reactor Monitor |")
 sleep(0.15)
 print("---------------------------")
 sleep(0.5)
 print()
 print()
 
-local reactor = peripheral.wrap("BigReactors-Reactor_0")
+local reactor = peripheral.wrap("BigReactors-Reactor_1")
 print("Wrapped reactor...")
 
-local monitor = peripheral.wrap("monitor_0")
+local monitor = peripheral.wrap("monitor_1")
 print("Wrapped monitor...")
 
 --monitor init stuff
@@ -25,22 +25,26 @@ monitor.setCursorPos(1,1)
 print("Initializing monitor etc...")
 
 --print to monitor splash...
-monitor.write("---------------------------")
-sleep(0.15)
+monitor.setTextColor(colors.red)
+monitor.write("---------------------------------------")
+sleep(0.2)
 monitor.setCursorPos(1,2)
-monitor.write("| Magic's Reactor Control |")
-sleep(0.15)
+monitor.setTextColor(colors.purple)
+monitor.write("|       Magic's Reactor Monitor       |")
+sleep(0.2)
 monitor.setCursorPos(1,3)
-monitor.write("---------------------------")
-sleep(0.5)
+monitor.setTextColor(colors.red)
+monitor.write("---------------------------------------")
+sleep(0.2)
 
 --output fuel level
+monitor.setTextColor(colors.white)
 monitor.setCursorPos(1,4)
 monitor.write("Fuel Level: ")
 monitor.setCursorPos(1,5)
 monitor.setTextColor(colors.yellow)
 monitor.write(reactor.getFuelAmount())
-monitor.write("%")
+monitor.write(" mB")
 print("Output fuel level...")
 
 --reset cursor pos
@@ -48,13 +52,21 @@ monitor.setCursorPos(1,6)
 monitor.setTextColor(colors.white)
 print("Move cursor down...")
 
---output fuel temperature
+--fuel temperature control and moooore
 monitor.write("Fuel Temperature: ")
-monitor.setCursorPos(1,7)
 monitor.setTextColor(colors.red)
-local mathme 
-mathme = math.floor(reactor.getFuelTemperature() + 0.5)
-monitor.write(mathme)
-monitor.write("c")
+print(reactor.getFuelTemperature())
+
+if tonumber(reactor.getFuelTemperature()) > 2000 then
+monitor.setCursorPos(1,18)
+monitor.write("!! OVERHEATING, LOWERING CONTROL ROD !!")
+local CtrlRodLvl = tonumber(reactor.getControlRodLevel(0))
+reactor.setAllControlRodLevels(CtrlRodLvl + 10)
+end
+
+monitor.setTextColor(colors.cyan)
+monitor.setCursorPos(1,7)
+monitor.write(math.floor(reactor.getFuelTemperature()))
+monitor.write("*c/2000*c MAX")
 print("Output fuel temperature...")
 
